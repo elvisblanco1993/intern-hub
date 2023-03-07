66d3805c464bba8ec9b4a74c3a1a4312d1e10fe7
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-06 21:27:15 -0500

    Created base working files for core behavior

---
+++ app/Http/Livewire/Admin/Subscriber/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Subscriber;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.subscriber.index');
+    }
+}
---
+++ app/Http/Livewire/Website/Subscriber/Unsubscribe.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Website\Subscriber;
+
+use Livewire\Component;
+
+class Unsubscribe extends Component
+{
+    public function render()
+    {
+        return view('livewire.website.subscriber.unsubscribe');
+    }
+}
