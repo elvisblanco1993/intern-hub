76a24c550e919ba0932d077762b694e3990a7185
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-29 10:31:39 -0400

    Added blur to improve visibility

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,11 @@
-<div style="background-image: url({{ asset('/static_assets/login_background.jpg')}})" class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-center bg-cover">
-    <div>
-        {{ $logo }}
-    </div>
+<div style="background-image: url({{ asset('/static_assets/login_background.jpg')}})" class="h-screen min-h-screen bg-center bg-cover">
+    <div class="h-full flex flex-col sm:justify-center items-center pt-6 sm:pt-0 backdrop-blur">
+        <div>
+            {{ $logo }}
+        </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
     </div>
 </div>
