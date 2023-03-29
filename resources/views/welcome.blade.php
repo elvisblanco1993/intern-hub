1ce6d4608cf6041c6394adec19b215d4747068ef
Elijahx <elijahkhazzouh@live.com>
2023-03-29 00:30:41 -0400

    Feat: welcome page implementation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,57 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-guest-layout>
+    <div class="h-screen bg-cover bg-center" style="background-image: url({{ asset('images/image3.png') }})">
+        <div class="h-full">
+            <div class="h-full px-4 sm:px-16 md:px-24 lg:px-48">
+                <div class="h-full flex items-center max-w-4xl">
+                    <div>
+                        <h1 class="text-white font-black text-7xl">
+                            Find the internship that works for you
+                        </h1>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <!-- This line adds a link to the app.css file in the HTML head section of the page.
-
-        With these changes in place, the image you added should now be displayed as the background of the welcome page.-->
-        <style>
-        body {
-            background-image: url('{{ asset('images/image3.png') }}');
-        }
-        .heading-wrapper {
-            width: 50%; /* adjust as necessary */
-            color: white;
-            margin: 30% 5%;
-            justify-content: center;
-            align-items: center;
-            height: auto;
-        }
-        .heading-wrapper h1 {
-            word-wrap: break-word;
-            text-align: left;
-            font-size: xxx-large;
-        }
-        .orange-button {
-            background-color: #F94D39;
-            color: white;
-            border-radius: 10px;
-            padding: 15px 60px;
-            font-size: 16px;
-            border: none;
-            cursor: pointer;
-            transition: background-color 0.3s ease-in-out;
-        }
-
-        .orange-button:hover {
-            background-color: #ff8533;
-        }
-        </style>
-
-    </head>
-    
-    <body>
-        <div class="heading-wrapper">
-            <h1>Find the internship that works for you</h1>
-            <button class="orange-button">START YOUR SEARCH</button>
-            
+                        <button class="h-full bg-orange-600 mt-6 inline-block px-8 py-4 rounded-lg uppercase bg-gradient-to-t from-intern to-intern/80 text-white font-bold">Start your search</button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+</x-guest-layout>
